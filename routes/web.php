1a3f8ce5bd7206a68107bd4d7d6dbb7374ff7ab9
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-31 10:14:58 +0700

    Fixinf routing

--- routes/web.php
+++ routes/web.php
@@ -20,32 +20,32 @@ Route::get('api-doc', function() {
 })->name('api.doc');
 
 // Login - Logout ADMIN
-Route::get('admin-login', 'backend\LoginController@login')->name('user.login');
-Route::post('admin-auth/login', 'backend\LoginController@doLogin')->name('user.auth.login');
-Route::get('admin-logout', 'backend\LoginController@doLogout')->name('user.logout');
+Route::get('admin-login', 'Backend\LoginController@login')->name('user.login');
+Route::post('admin-auth/login', 'Backend\LoginController@doLogin')->name('user.auth.login');
+Route::get('admin-logout', 'Backend\LoginController@doLogout')->name('user.logout');
 
 //Dashboard
 Route::group(['middleware'=>'auth'], function() {
-  Route::get('dashboard-admin', 'backend\LoginController@dashboardAdmin')->name('dashboard.admin');
+  Route::get('dashboard-admin', 'Backend\LoginController@dashboardAdmin')->name('dashboard.admin');
 });
 
 //Pengelolaan Kost
 Route::group(['middleware'=>'auth'], function() {
-  Route::get('kost-index', 'backend\KostController@index')->name('back.kost.index');
-  Route::get('kost-create', 'backend\KostController@create')->name('back.kost.create');
-  Route::post('kost-store', 'backend\KostController@store')->name('back.kost.store');
-  Route::get('kost-edit/{id}', 'backend\KostController@edit')->name('back.kost.edit');
-  Route::patch('kost-update/{id}', 'backend\KostController@update')->name('back.kost.update');
-  Route::get('kost-delete/{id}', 'backend\KostController@destroy')->name('back.kost.destroy');
+  Route::get('kost-index', 'Backend\KostController@index')->name('back.kost.index');
+  Route::get('kost-create', 'Backend\KostController@create')->name('back.kost.create');
+  Route::post('kost-store', 'Backend\KostController@store')->name('back.kost.store');
+  Route::get('kost-edit/{id}', 'Backend\KostController@edit')->name('back.kost.edit');
+  Route::patch('kost-update/{id}', 'Backend\KostController@update')->name('back.kost.update');
+  Route::get('kost-delete/{id}', 'Backend\KostController@destroy')->name('back.kost.destroy');
 });
 
 
 //Pengelolaan Pemilik
 Route::group(['middleware'=>'auth'], function() {
-  Route::get('pemilik-index', 'backend\PemilikController@index')->name('back.pemilik.index');
-  Route::get('pemilik-create', 'backend\PemilikController@create')->name('back.pemilik.create');
-  Route::post('pemilik-store', 'backend\PemilikController@store')->name('back.pemilik.store');
-  Route::get('pemilik-edit/{id}', 'backend\PemilikController@edit')->name('back.pemilik.edit');
-  Route::patch('pemilik-update/{id}', 'backend\PemilikController@update')->name('back.pemilik.update');
-  Route::get('pemilik-delete/{id}', 'backend\PemilikController@destroy')->name('back.pemilik.destroy');
+  Route::get('pemilik-index', 'Backend\PemilikController@index')->name('back.pemilik.index');
+  Route::get('pemilik-create', 'Backend\PemilikController@create')->name('back.pemilik.create');
+  Route::post('pemilik-store', 'Backend\PemilikController@store')->name('back.pemilik.store');
+  Route::get('pemilik-edit/{id}', 'Backend\PemilikController@edit')->name('back.pemilik.edit');
+  Route::patch('pemilik-update/{id}', 'Backend\PemilikController@update')->name('back.pemilik.update');
+  Route::get('pemilik-delete/{id}', 'Backend\PemilikController@destroy')->name('back.pemilik.destroy');
 });
