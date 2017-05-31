1bd7f7c130a8227f00c02fc17a69fe497802777e
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-31 09:34:30 +0700

    Remove toolbar icon

--- resources/views/backend/template.blade.php
+++ resources/views/backend/template.blade.php
@@ -10,7 +10,7 @@
   	<meta name="author" content="KeyKost" />
 
     <title>KeyKost</title>
-    <link rel="icon" href="{{ asset('uploads/Logo/logo_tabbrowser.png') }}" type="image" sizes="16x16"> <!------------------------------------------ ICON-------------->
+    <!-- <link rel="icon" href="{{ asset('uploads/Logo/logo_tabbrowser.png') }}" type="image" sizes="16x16"> <!------------------------------------------ ICON--------------> -->
 
     <!----------------------------------------------------Buat Dashboard(index.html)---------->
     <!-- Bootstrap -->
