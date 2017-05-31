616e428b41dafed93299c65af79e7d852e8d62eb
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-31 10:15:52 +0700

    root routing to login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('user.login');
 });
 
 Route::get('api-doc', function() {
